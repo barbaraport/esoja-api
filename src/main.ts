3533ff16fbe0ab5352f45589073e700eac2f41e6
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-08-24 07:16:40 -0300

    fixing some details to run the project locally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { globalAppConfig } from './utils/functions/global.config.fn';
+import ip from 'ip';
 
 async function bootstrap() {
+  const ipAddress: string = ip.address();
+  const port: string | number = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
 
   globalAppConfig(app);
@@ -13,9 +16,9 @@ async function bootstrap() {
     methods: 'GET, POST, PUT, DELETE, UPDATE, OPTIONS',
   });
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(port, ipAddress);
 
-  console.log('Server on');
+  console.log(`Server running at ${ipAddress}:${port}`);
   console.log(`env: .env.${process.env.NODE_ENV}`);
 }
 bootstrap();
