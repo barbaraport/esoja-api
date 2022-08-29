156621718455d77ef613c106a5f2249c6066a0f8
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-08-29 08:14:15 -0300

    changing the ip address to the variable

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     methods: 'GET, POST, PUT, DELETE, UPDATE, OPTIONS',
   });
 
-  await app.listen(port, 'localhost');
+  await app.listen(port, ipAddress);
 
   console.log(`Server running at ${ipAddress}:${port}`);
   console.log(`env: .env.${process.env.NODE_ENV}`);
